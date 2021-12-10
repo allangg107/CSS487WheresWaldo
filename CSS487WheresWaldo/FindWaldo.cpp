c94ac2290b30eccc7088243cc7f7fc0f3ee3c2f5
allangg107 <allangenarigaarden@hotmail.com>
2021-12-10 14:35:19 -0800

    reworked to test each image

--- CSS487WheresWaldo/FindWaldo.cpp
+++ CSS487WheresWaldo/FindWaldo.cpp
@@ -13,17 +13,19 @@
 using namespace std;
 using namespace cv;
 
-// ------------------------------------ main --------------------------------------
+// ------------------------------------ testCase --------------------------------------
 
-// purpose: recognize and label Waldo in a given "Where's Waldo" image
-// preconditions: Using a "Where's Waldo" image as well as a template image for Waldo
-// postconditions: Waldo is located and boxed on a copy of the original image
+// purpose: test a given case
+// preconditions: test is using valid images
+// postconditions: outputs the result of each test case, with Waldo being boxed
+// This method was inspired by opencv's documentation on how to perform template matching:
+//	https://docs.opencv.org/3.4/de/da9/tutorial_template_matching.html
 
 // --------------------------------------------------------------------------------------
-int main()
+void testCase(string puzzleName, string templateName)
 {
-	Mat waldoPuzzle = imread("waldo_puzzle_06.png");
-	Mat waldoTemplate = imread("waldo_template_06.png");
+	Mat waldoPuzzle = imread(puzzleName);
+	Mat waldoTemplate = imread(templateName);
 
 	Mat result = waldoPuzzle.clone();
 
@@ -50,4 +52,42 @@ int main()
 	imshow("result.png", result);
 
 	waitKey(0);
+}
+
+// ------------------------------------ main --------------------------------------
+
+// purpose: recognize and label Waldo in a given "Where's Waldo" image
+// preconditions: Using an existing "Where's Waldo" image as well as a template image for Waldo
+// postconditions: Waldo is located and boxed on a copy of the original image
+
+// --------------------------------------------------------------------------------------
+int main()
+{
+	vector<string> puzzles;
+	vector<string> templates;
+
+	// populate test vectors
+	puzzles.push_back("waldo_puzzle_01.png");
+	puzzles.push_back("waldo_puzzle_02.png");
+	puzzles.push_back("waldo_puzzle_03.png");
+	puzzles.push_back("waldo_puzzle_04.png");
+	puzzles.push_back("waldo_puzzle_05.png");
+	puzzles.push_back("waldo_puzzle_06.png");
+	puzzles.push_back("waldo_puzzle_07.png");
+	puzzles.push_back("waldo_puzzle_08.png");
+
+	templates.push_back("waldo_template_01.png");
+	templates.push_back("waldo_template_02.png");
+	templates.push_back("waldo_template_03.png");
+	templates.push_back("waldo_template_04.png");
+	templates.push_back("waldo_template_05.png");
+	templates.push_back("waldo_template_06.png");
+	templates.push_back("waldo_template_07.png");
+	templates.push_back("waldo_template_08.png");
+
+	// run each test case
+	for (int i = 0; i < puzzles.size(); i++)
+	{
+		testCase(puzzles[i], templates[i]);
+	}
 }
